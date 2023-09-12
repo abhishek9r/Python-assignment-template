d2b320f833b0076305705becf222e731d32251ca
SpyDie <anaadidubey306@gmail.com>
2023-09-12 22:34:11 +0530

    IO WRAPPER FINALLY CREATED

---
+++ IOWrapper.py
@@ -0,0 +1,14 @@
+from io import StringIO 
+class IOtranferObject:
+    def __init__(self):
+        self.inBuffer=StringIO("")
+        self.outBuffer=StringIO("")
+    def input():
+        return self.inBuffer
+    def check(o):
+        if (self.inBuffer==o.inBuffer) and (self.outBuffer==o.outBuffer):
+            return True
+        else :
+            return False
+
+    
